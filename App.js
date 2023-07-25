e8b36698cd834e839ad3f166a5dbd1e44f878f99
Valentin Raduti <valentin@thedotin.ro>
2023-07-25 09:49:37 +0300

    first

--- App.js
+++ App.js
@@ -2,6 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const ws = new WebSocket('https://test.doctorapp.ro/doctor-app-websocket'); 
+  // const ws = new WebSocket('http://192.168.0.210:8081/doctor-app-websocket'); 
+  ws.onerror = console.error;
+  ws.onopen = console.log;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
